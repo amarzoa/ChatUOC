4ee9919be6dde98cef61913833779573de6c8757
Adrian <amarzoa@gmail.com>
2021-12-11 20:55:53 +0100

    Primer commit

---
+++ 1.py
@@ -0,0 +1,45 @@
+import pickle
+import nltk 
+from nltk.stem import WordNetLemmatizer
+import json
+import numpy
+import tensorflow
+import numpy as np  
+
+from tensorflow import keras
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import Dense, Activation, Dropout 
+from tensorflow.python.keras.optimizers import SGD  
+
+randomwords=[]  
+classes = []  
+documents = []
+ignore_words = ['?', '!']
+data_file = open('intents.json').read()  
+words=""
+pik=pickle
+
+intents = json.loads(data_file)
+
+lemmatizer = WordNetLemmatizer()
+
+for intent in intents['intents']:   
+    for pattern in intent['patterns']:   
+        #tokenize each word   
+        w = nltk.word_tokenize(pattern)   
+        w.extend(w) #add documents in the corpus   
+        documents.append((w, intent['tag']))   # add to our classes list   
+        if intent['tag'] not in classes:   
+            classes.append(intent['tag'])
+
+# lemmatize, lower each word and remove duplicates  
+w = [lemmatizer.lemmatize(w.lower())  for w in words if w not in ignore_words]   
+w = sorted(list(set(words)))# sort classes  
+classes = sorted(list(set(classes))) # documents = combination between patterns and intents  
+print(len(documents), "documents")# classes = intents  
+print(len(classes), "classes", classes)# words = all words, vocabulary  
+print(len(w), "unique lemmatized words", words)  
+pickle.dump(w,open('words.pkl','wb'))  
+pickle.dump(classes,open('classes.pkl','wb'))
+
+print("Bot: Hola, Mi nombre es BOTUOC")
---
+++ chatgui.py
@@ -0,0 +1,77 @@
+
+import nltk, json, random, pickle
+from nltk.stem import WordNetLemmatizer
+from tensorflow.python.keras.engine.sequential import Sequential
+lemmatizer = WordNetLemmatizer()
+import pickle
+import numpy as np
+from tensorflow.keras.models import load_model
+import tensorflow as tf
+import os
+
+
+
+#ruta=os.getcwd() + '\chatbot_model'
+
+#archivo=open(ruta)
+#ruta=path('\chatbot_model.h5')
+#ruta="C:\Users\Adrian\Desktop\python\TFG"
+model=load_model(os.getcwd() + '\chatbot_model')
+#model = load_model('C:\Users\Adrian\Desktop\python\TFG\chatbot_model.h5')
+
+
+intents = json.loads(open('intents.json').read())
+words = pickle.load(open('words.pkl','rb'))
+classes = pickle.load(open('classes.pkl','rb'))
+
+# preprocessamento input utente
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
+    return sentence_words
+
+# creazione bag of words
+def bow(sentence, words, show_details=True):
+    sentence_words = clean_up_sentence(sentence)
+    bag = [0]*len(words)
+    for s in sentence_words:
+        for i,w in enumerate(words):
+            if w == s:
+                bag[i] = 1
+                if show_details:
+                    print ("found in bag: %s" % w)
+    return(np.array(bag))
+
+def calcula_pred(sentence, model):
+    p = bow(sentence, words,show_details=False)
+    res = model.predict(np.array([p]))[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
+    # sort by strength of probability
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
+    return return_list
+
+def getRespuesta(ints, intents_json):
+    tag = ints[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if(i['tag']== tag):
+            result = random.choice(i['responses'])
+            break
+    return result
+
+def inicia(msg):
+    ints = calcula_pred(msg, model)
+    res = getRespuesta(ints, intents)
+    return res
+
+usuario = ''
+print('Bienvenido al Chat UOC TFG, para salir escribe "Exit"')
+
+while usuario != 'Exit':
+    usuario = str(input(""))
+    res = inicia(usuario)
+    print('BOT UOC:' + res)
